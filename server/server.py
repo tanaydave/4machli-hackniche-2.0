830a0e7dc21f23789171f7c123848d36abc04acf
devshah <devshah0404@gmail.com>
2024-02-17 16:31:10 +0530

    reviewAnalysis

--- server/server.py
+++ server/server.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pandas as pd
 import math,random
-import salesAnalysis
+import salesAnalysis,reviewAnalysis
 app = Flask(__name__)
 CORS(app)
 
@@ -15,7 +15,14 @@ def promotions():
     except Exception as e:
         return jsonify({'error': str(e)})
 
+@app.route('/reviews', methods=['GET'])
+def reviews():
+    try:
+        sentiment_value,filtered_words = reviewAnalysis.get_review_analysis()
+        return {'sentiment_value': sentiment_value ,'filtered_words':filtered_words}
 
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
     app.run(port=5000)
